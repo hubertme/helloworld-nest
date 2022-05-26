b9d8b45d773c5ed41db6be1dc523e1a8293cda69
Hubert Wang <hubert.tatra@gmail.com>
2022-05-26 17:04:24 +0700

    Add responses + alter test endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
+import ServerResponse from 'responses/server_response';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,7 +7,8 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(): ServerResponse {
+    const result = this.appService.getHello();
+    return ServerResponse.Success(result);
   }
 }
